1036654e41791015de22f0118d0357eca1d0db01
fanxu wang <fanxu.wang@thoughtworks.com>
2024-07-25 16:08:14 +0800

    test: unit test coverage setup

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,15 @@
 import type { Config } from "@jest/types";
 
+const baseDir = "<rootDir>/src/app";
+const baseTestDir = "<rootDir>/src/test";
+
 const config: Config.InitialOptions = {
   preset: "ts-jest",
   testEnvironment: "node",
   verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: [`${baseDir}/**/*.ts`],
+  testMatch: [`${baseTestDir}/**/*.ts`],
 };
 
 export default config;
